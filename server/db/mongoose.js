a174c447635a8af62dd36274fc5989aa2e138048
Phuong Phan <phuongphan0608@gmail.com>
2018-06-26 19:02:02 +0700

    a

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp-Mongoose');
-mongoose.connect('mongodb://user1:rlGmFO3rX3VQ@ds119161.mlab.com:19161/todoapp-cloud');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp-Mongoose');
+// mongoose.connect('mongodb://user1:rlGmFO3rX3VQ@ds119161.mlab.com:19161/todoapp-cloud');
 
 module.exports = {
   mongoose
