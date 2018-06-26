2335d4c4186112a8b96835a6edc030370892bef2
Phuong Phan <phuongphan0608@gmail.com>
2018-06-26 12:33:09 +0700

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-
-
+const express = require('express');
+const bodyParser = require('body-parser');
+const hsb = require('hbs');
+const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} =require('./models/user');
 
 var app = express();
+app.set('view engine','hbs');
 
 //post route,
 //Parse incoming request bodies in a middleware before your handlers, available under the req.body property
@@ -26,6 +27,41 @@ app.post('/todos',(req,res) => {
 });
 });
 
+app.get('/todos', (req,res) =>{
+  Todo.find().then((todos) => {
+    // var todos = res.send(JSON.stringify({todos}));
+    res.send({todos});
+    // res.render('todos.hbs',(text) => {
+    //   console.log(res.body);
+    // })
+
+  }
+  ,(e) => {
+    res.status(400).send(e)
+  });
+});
+
+
+app.get('/todos/:id',(req, res) => {
+  // res.send(req.params);
+  var id = req.params.id;
+
+  // Valid id using isValid
+  // 404 - send back empty send
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send('Object id is invalid');
+  }
+
+  Todo.findById(id).then((todo) => {
+
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.status(200).send(JSON.stringify({todo}, undefined,2));
+  }).catch((e) => {
+   res.status(400).send();
+ });
+});
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
